20788c536166f34fcd2ef8888a898613c6f42067
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-21 18:48:44 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 gitbook build
 gitbook pdf
 mv book.pdf
-fp -r _book/* ../sttp-book.github.io/
+cp -r _book/* ../sttp-book.github.io/
 rm -rf ../sttp-book.github.io/drawio 
 rm -rf ../sttp-book.github.io/answers 
 rm -rf ../sttp-book.github.io/includes 
 rm ../sttp-book.github.io/*.md
+rm ../sttp-book.github.io/deploy.sh
 cp book.pdf ../sttp-book.github.io/sttp.pdf
 
