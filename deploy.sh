489b568e256d09fbae559a3a1fdcd1cb00d1571c
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-22 17:51:29 +0100

    one more folder to clean up

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ cp -r _book/* ../sttp-book.github.io/
 rm -rf ../sttp-book.github.io/drawio 
 rm -rf ../sttp-book.github.io/answers 
 rm -rf ../sttp-book.github.io/includes 
+rm -rf ../sttp-book.github.io/latex-conf
 rm ../sttp-book.github.io/*.sh
 cp book.pdf ../sttp-book.github.io/sttp.pdf
 
