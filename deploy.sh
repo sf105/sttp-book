370e588f221fa6c7b914c9da7af753189ef0d8c7
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-22 15:54:22 +0100

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 gitbook build
 gitbook pdf
+rm -rf ../sttp-book.github.io/assets/ ../sttp-book.github.io/chapters/ ../sttp-book.github.io/gitbook/ ../sttp-book.github.io/gitbook_to_pdf.sh ../sttp-book.github.io/index.html ../sttp-book.github.io/search_index.json
 cp -r _book/* ../sttp-book.github.io/
 rm -rf ../sttp-book.github.io/drawio 
 rm -rf ../sttp-book.github.io/answers 
