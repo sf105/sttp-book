28b1f122f2b4b0b25e849c5918214207d7ecda84
wouterpolet <w.h.polet@student.tudelft.nl>
2020-03-27 01:09:23 +0100

    Add missing drawio files and convert drawio to pdf when generating book

--- gitbook_to_pdf.sh
+++ gitbook_to_pdf.sh
@@ -8,6 +8,13 @@ if [ -d "$GITBOOK_REP" ]; then
 
   echo "Entering directory '$GITBOOK_REP'..."
   cd $GITBOOK_REP
+  mkdir temp
+  # Add other svg files
+  find chapters -type f -name '*.svg' | while read line; do
+    rsvg-convert -f pdf -o temp/$(basename "${line%.*}").drawio.pdf $line
+  done
+  # Convert drawio files to pdf
+  drawio --crop -xtrf pdf -o temp/ drawio/
   if [ -f "$SUMMARY_FILE" ]; then
     # read summary and get texts by order in a single big file
     # we replace: 
@@ -23,6 +30,7 @@ if [ -d "$GITBOOK_REP" ]; then
       perl -pe 's/{% endhint %}/\n***/g' | \
       perl -pe 's/{% include "\/includes\/youtube.md" %}//g' | \
       perl -pe 's/{% set video_id = "([A-Za-z0-9-_]*)" %}/***\nWatch our video on YouTube:\n\nhttp:\/\/www.youtube.com\/embed\/\1\n\n***/g' | \
+      perl -pe "s/img.*\/(.*)\.svg/temp\/\1.drawio.pdf/g" | \
       pandoc -f markdown \
               --variable fontsize=11pt \
               --variable=geometry:b5paper \
@@ -30,12 +38,14 @@ if [ -d "$GITBOOK_REP" ]; then
               --variable documentclass=book \
               -H latex-conf/head.tex \
               -V subparagraph \
-              --resource-path="chapters/getting-started/:chapters/intelligent-testing:chapters/pragmatic-testing:chapters/testing-techniques:chapters/appendix" \
+              --resource-path="./:chapters/getting-started/:chapters/intelligent-testing:chapters/pragmatic-testing:chapters/testing-techniques:chapters/appendix" \
              --toc --toc-depth=3 --pdf-engine=xelatex -o book.pdf
   else
     echo "File '$SUMMARY_FILE' does not exist"
   fi
+  # Remove drawio PDFs
+  rm -r temp
 else
-echo "Directory '$GITBOOK_REP' does not exist"
+  echo "Directory '$GITBOOK_REP' does not exist"
 fi
 
